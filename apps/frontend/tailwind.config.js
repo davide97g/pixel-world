16ec629e8580824473c825155ba50fd8ab2d76d3
Davide Ghiotto <davide.ghiotto@bitrock.it>
2025-01-18 11:59:07 +0100

    refactor(frontend): vite webapp

--- apps/frontend/tailwind.config.js
+++ apps/frontend/tailwind.config.js
@@ -1,51 +1,50 @@
 /** @type {import('tailwindcss').Config} */
-// eslint-disable-next-line @typescript-eslint/no-require-imports, no-undef
-import { nextui } from '@nextui-org/react';
+import { nextui } from "@nextui-org/react";
 
 export default {
   content: [
-    './src/**/*.{html,js,ts,jsx,tsx}',
-    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
+    "./src/**/*.{html,js,ts,jsx,tsx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
-  darkMode: 'class',
+  darkMode: "class",
   plugins: [
     nextui({
       themes: {
-        'purple-dark': {
-          extend: 'dark', // <- inherit default values from dark theme
+        "purple-dark": {
+          extend: "dark", // <- inherit default values from dark theme
           colors: {
-            background: '#0D001A',
-            foreground: '#ffffff',
+            background: "#0D001A",
+            foreground: "#ffffff",
             primary: {
-              50: '#3B096C',
-              100: '#520F83',
-              200: '#7318A2',
-              300: '#9823C2',
-              400: '#c031e2',
-              500: '#DD62ED',
-              600: '#F182F6',
-              700: '#FCADF9',
-              800: '#FDD5F9',
-              900: '#FEECFE',
-              DEFAULT: '#DD62ED',
-              foreground: '#ffffff',
+              50: "#3B096C",
+              100: "#520F83",
+              200: "#7318A2",
+              300: "#9823C2",
+              400: "#c031e2",
+              500: "#DD62ED",
+              600: "#F182F6",
+              700: "#FCADF9",
+              800: "#FDD5F9",
+              900: "#FEECFE",
+              DEFAULT: "#DD62ED",
+              foreground: "#ffffff",
             },
-            focus: '#F182F6',
+            focus: "#F182F6",
           },
           layout: {
-            disabledOpacity: '0.3',
+            disabledOpacity: "0.3",
             radius: {
-              small: '4px',
-              medium: '6px',
-              large: '8px',
+              small: "4px",
+              medium: "6px",
+              large: "8px",
             },
             borderWidth: {
-              small: '1px',
-              medium: '2px',
-              large: '3px',
+              small: "1px",
+              medium: "2px",
+              large: "3px",
             },
           },
         },
