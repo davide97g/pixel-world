240f40de7d850088946375ceeaf621e129e73d5f
Davide Ghiotto <davide.ghiotto@bitrock.it>
2025-01-26 19:30:16 +0100

    rename use toast hook

--- apps/frontend/src/pages/Register.tsx
+++ apps/frontend/src/pages/Register.tsx
@@ -12,7 +12,7 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { useAuth } from "@/context/AuthProvider";
-import { useToast } from "@/hooks/use-toast";
+import { useToast } from "@/hooks/useToast";
 import { AuthenticationService } from "@/services/AuthenticationService";
 import { useEffect, useState, type FormEvent } from "react";
 import { useNavigate } from "react-router";
